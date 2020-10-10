e651aa1d46164def8829c97bf896abb21527f66b
taomee517 <taomee517@gmail.com>
2020-10-10 18:47:40 +0800

    project init

---
+++ src/main/java/org/example/mosquito/SpringbootMosquitoApplication.java
@@ -0,0 +1,17 @@
+package org.example.mosquito;
+
+/**
+ * @author 罗涛
+ * @title SpringbootEmqApplication
+ * @date 2020/10/10 17:09
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMosquitoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMosquitoApplication.class);
+    }
+}
---
+++ src/main/java/org/example/mosquito/controller/IndexController.java
@@ -0,0 +1,20 @@
+package org.example.mosquito.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 罗涛
+ * @title IndexController
+ * @date 2020/10/10 17:12
+ */
+@RestController
+@RequestMapping(value = "/demo")
+public class IndexController {
+
+    @RequestMapping(value = "/index",  method = RequestMethod.GET)
+    public String index(){
+        return "Hello, Mosquito!";
+    }
+}
